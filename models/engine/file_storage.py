7c5ba8dd9d678e33925daf41f4c2b09c41cf6b21
Aymenbs22 <aymenbenslima1@gmail.com>
2022-10-16 13:25:03 +0100

    add del

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,6 +10,9 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            new_dict = {}
+            return new_dict
         return FileStorage.__objects
 
     def new(self, obj):
