5a154abe424eb6ebfe79627b535fe8ddae8bf0c4
Aymenbs22 <aymenbenslima1@gmail.com>
2022-10-16 13:46:51 +0100

    Bash script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-sudo tee /data/web_static/releases/test/index.html
+echo "0" |sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo service nginx start
